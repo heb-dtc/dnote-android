2af247a092f91ad8d5e573c4b67a0f16c49e5d07
Florent NOEL <florent.noel@protonmail.com>
2020-01-26 21:55:17 +0100

    AddNoteActivity skeleton

---
+++ app/src/main/java/com/heb/dnote/AddNoteActivity.kt
@@ -0,0 +1,18 @@
+package com.heb.dnote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.heb.dnote.note.Note
+import com.heb.dnote.note.NoteRepository
+
+class AddNoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
